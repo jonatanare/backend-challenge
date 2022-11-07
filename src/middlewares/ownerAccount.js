6a9a023dc9f6f380a430a5315f2b69c65a5501da
Cintia Ruiz <cintiaruizv@gmail.com>
2022-11-06 19:31:08 -0600

    i forgot to change of branch again

--- src/middlewares/ownerAccount.js
+++ src/middlewares/ownerAccount.js
@@ -3,19 +3,18 @@ import { Post } from '../models/posts.model.js'
 import { Comment } from '../models/comment.model.js'
 import { StatusHttp } from '../libs/statusHttp.js'
 
+
 function accessOwnerAccount (request, response, next) {
   try {
     const { userCurrent } = request /* token decoded */
 
     const idUser = request.params.id /* se cambia de nombre a rutas idCompany a id URL */
-    console.log('id del usuario logeado', userCurrent)
-    console.log('id del usuario a editar ', idUser)
+   
 
     if (userCurrent !== idUser) throw new Error('You can only edit your own Account')
 
     next()
   } catch (error) {
-    console.log(error)
     response.status(403)
     response.json({
       success: false,
@@ -24,7 +23,7 @@ function accessOwnerAccount (request, response, next) {
   }
 }
 
-async function accessOwnerPostsOrComments (request, response, next) {
+async function accessOwnerPostsOrComments(request, response, next) {
   try {
     const { userCurrent } = request /* id de user current */
 
@@ -33,14 +32,12 @@ async function accessOwnerPostsOrComments (request, response, next) {
     const postOrCommentId = await Post.findById(id) || await Comment.findById(id)
     if (!postOrCommentId) throw new StatusHttp('Request not found!')
     const idUser = postOrCommentId.author._id.valueOf()
-    console.log('id del usuario logeado', userCurrent)
-    console.log('id del usuario a editar ', idUser)
+   
 
     if (userCurrent !== idUser) throw new Error('You can only edit your own Account')
 
     next()
   } catch (error) {
-    console.log(error)
     response.status(403)
     response.json({
       success: false,
@@ -49,7 +46,8 @@ async function accessOwnerPostsOrComments (request, response, next) {
   }
 }
 
-export {
+
+export { 
   accessOwnerAccount,
-  accessOwnerPostsOrComments
-}
+  accessOwnerPostsOrComments,
+ }
